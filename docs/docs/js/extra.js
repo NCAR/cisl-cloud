0e1e34dcbbc7154bf20686c2cbc44ee2312fc24f
Nick Cote <ncote@ucar.edu>
2023-04-28 12:19:41 -0600

    fixing code blocks with css

--- docs/docs/js/extra.js
+++ docs/docs/js/extra.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // https://clipboardjs.com/
-    var selectors = document.querySelectorAll('pre code');
+    var selectors = document.querySelectorAll('code');
     var copyButton = '<div class="clipboard"><span class="btn btn-neutral btn-clipboard" title="Copy to clipboard">Copy</span></div>';
     Array.prototype.forEach.call(selectors, function(selector){
       selector.insertAdjacentHTML('beforebegin', copyButton);
