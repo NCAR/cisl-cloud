c0309f4aa15dfd32065f3b68657838469ca8d32b
Nick Cote <ncote@ucar.edu>
2023-06-05 16:06:06 -0600

    Change conda dir filter

--- configs/jupyter/base-notebook/scripts/jupyter_notebook_config.py
+++ configs/jupyter/base-notebook/scripts/jupyter_notebook_config.py
@@ -57,4 +57,4 @@ if "NB_UMASK" in os.environ:
 
 # Configure nb_conda_kernels to avoid registering jupyter kernels in our conda
 # environment again.
-c.CondaKernelSpecManager.env_filter = f'.*envs/{os.environ["CONDA_ENV"]}.*'
+c.CondaKernelSpecManager.env_filter = '/opt/conda/*envs'
